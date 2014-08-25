d60407a50169a543ddd2d3cbf30c0394a6cf0db1
AllaHoffman <sm1th303@gmail.com>
2014-08-25 17:11:32 -0400

    Update analyzercsv.rb

--- analyzercsv.rb
+++ analyzercsv.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 CSV.foreach('C:\Users\ahoffman\Documents\TestAnalysis\node.csv') do |row|
   name = row["Test"]
-  status = row["Status"] #do stuff to
+  status = row["Status"] #do stuff to; test sub-groupings have '( # of tests' while supra groupings have '<[name]>'
   speed = row["Status"]
 end
 
