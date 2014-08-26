e5a77ab0d373eeb85624116cc8f769f46dcbb8fc
AllaHoffman <sm1th303@gmail.com>
2014-08-25 21:35:07 -0400

    Update analyzercsv.rb
    Accurately breaks out slowest individual tests, test groups, and supratest categories, given .csv created by converting VB test output from .txt to .csv as in example file

--- analyzercsv.rb
+++ analyzercsv.rb
@@ -1,11 +1,56 @@
 require 'csv'
 
-CSV.foreach('C:\Users\ahoffman\Documents\TestAnalysis\node.csv') do |row|
+@individual_tests = []
+
+@over_test = ""
+
+@supra_test = ""
+
+@overtests = []
+
+@supratests = []
+
+CSV.foreach('/Users/alla/Documents/programming/testresultparser-master/dataexample.csv',headers: true) do |row|
+  test_for_overclass = /\(\d+ tests\)/
+  test_for_supraclass = /<.+>/
+
   name = row["Test"]
-  status = row["Status"] #do stuff to; test sub-groupings have '( # of tests' while supra groupings have '<[name]>'
-  speed = row["Status"]
+  status = row["Status"].match(/\] .+/).to_s
+  speed = row["Status"].match(/\[\d+:\d\d.\d{3}\]/).to_s
+
+  if !(name.match(test_for_overclass)==nil)
+    if !(name.match(test_for_supraclass)==nil)
+      @supra_test = name
+
+      @supratests.push(name: name, status: status, speed: speed)
+    else
+      @over_test = name
+
+      @overtests.push(name: name, status: status, speed: speed, supratest: @supra_test)
+    end
+  else
+    @individual_tests.push(name: name, status: status, speed: speed, overtest: @over_test,supratest: @supra_test)
+  end
 end
 
+@individual_tests.sort!{|x,y| x[:speed] <=> y[:speed]}.reverse!
 
+@overtests.sort!{|x,y| x[:speed] <=> y[:speed]}.reverse!
 
-/\(\d+ tests\)/
+@supratests.sort!{|x,y| x[:speed] <=> y[:speed]}.reverse!
+
+puts "10 Slowest Tests Are:"
+
+@individual_tests[0..10].each {|x| puts x[:name], x[:speed]}
+puts
+
+puts "5 Slowest Test Groups Are:"
+
+@overtests[0..5].each {|x| puts x[:name], x[:speed]}
+puts
+
+puts "Slowest Over-arching Group Is:"
+
+puts @supratests[0][:name] + ',' + @supratests[0][:speed]
+
+puts 'done!'
